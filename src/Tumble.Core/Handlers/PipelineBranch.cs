19429838e00558fca8ba5fd37a1511916322dca1
Darren Davis <daz@eircom.net>
2018-08-22 00:19:13 +0100

    Pipeline additions

---
+++ src/Tumble.Core/Handlers/PipelineBranch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumble.Core.Handlers
+{
+    public class PipelineBranch : IPipelineHandler
+    {
+        public PipelineRequest Branch { get; set; }
+
+        public async Task InvokeAsync(PipelineContext context, PipelineDelegate next)
+        {
+            await Branch?.InvokeAsync(context);
+            await next.Invoke();
+        }
+    }
+}
