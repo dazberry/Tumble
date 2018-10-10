68abbfdbae04eb6c02a48351e92a2ed323c557d5
Darren Davis <daz@eircom.net>
2018-10-11 00:09:23 +0100

    Added DubinBikes API

---
+++ samples/PipelinedApi/PipelinedApi/Handlers/DublinBikes/ParseStationResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using PipelinedApi.Models;
+using Tumble.Core;
+
+namespace PipelinedApi.Handlers.DublinBikes
+{
+    public class ParseStationResponse : IPipelineHandler
+    {        
+        public async Task InvokeAsync(PipelineContext context, PipelineDelegate next)
+        {
+            if (context.GetFirst(out HttpResponseMessage responseMessage))
+            {
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var result = await responseMessage.Content.ReadAsStringAsync();
+                    var response = JsonConvert.DeserializeObject<DublinBikeStation>
+                        (result, new UnixDateTimeConverter());
+                    context.Add("response", response);                   
+                }
+            }
+            else
+                throw new PipelineDependencyException<HttpResponseMessage>(this);
+        }
+    }
+
+    
+}
---
+++ samples/PipelinedApi/PipelinedApi/Handlers/DublinBikes/SetEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tumble.Client.Extensions;
+using Tumble.Core;
+using Tumble.Core.Notifications;
+
+namespace PipelinedApi.Handlers.DublinBikes
+{
+    public class SetEndpoint : IPipelineHandler
+    {
+        public Uri BaseUrl { get; set; }
+
+        public async Task InvokeAsync(PipelineContext context, PipelineDelegate next)
+        {
+            if (context.Get("endpoint", out string endPoint))
+            {
+                if (BaseUrl == null)
+                {
+                    context.AddNotification(this, "Missing BaseUrl value");
+                    return;
+                }
+                var uri = BaseUrl.Append(endPoint);
+                context.Add(uri);
+                await next.Invoke();
+            }
+            else
+                context.AddNotification(this, "Missing endpoint information");
+        }
+    }
+}
